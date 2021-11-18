6fa1753720e5fe6a162ef667ff426ecf3e86d7f6
Damian Epsztein <damiepsz3@gmail.com>
2021-11-18 17:24:20 -0300

    first commit

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "gatsby-plugin-firebase" {
+  export function useFirebase(
+    callback: (firebase: typeof import("firebase")) => void | (() => void),
+    deps: any[]
+  ): void;
+
+  export var FirebaseContext: import("react").Context<
+    typeof import("firebase") | undefined
+  >;
+}
